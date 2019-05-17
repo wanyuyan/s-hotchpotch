f7134d4fe9e2d1c2e1c963ec2445f79d7be11dd5
wanyuyan <wanyy@zetyun.com>
2019-05-17 18:40:41 +0800

    structor

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,16 +1,25 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
   entry: '/src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 's-hotchpotch.bundle.js'
+    chunkFilename: '[name].[contenthash].bundle.js',
+    publicPath: '/',    // csdn url, prod
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.(css|scss)$/,
         use: [
           { loader: ['style-loader'](/loaders/style-loader) },
           {
@@ -21,7 +30,38 @@ module.exports = {
           },
           { loader: ['sass-loader'](/loaders/sass-loader) }
         ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
       }
-    ]
-  }
+    ],
+    optimization: {
+      runtimeChunk: 'single',
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all'
+          }
+        }
+      }
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Caching'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
